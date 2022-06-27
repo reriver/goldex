fe806f2cdcf49aab80d619661ab2bc05659be24b
Jorge <71904707+reriver@users.noreply.github.com>
2022-06-27 17:59:55 +0300

    42 2

--- goldex.go
+++ goldex.go
@@ -1,8 +1,18 @@
 package goldex
 
+var localGoldexVar int
+
 func return42(i int) int {
 	return 42
 }
 func add42(i int) int {
 	return i + 42
+}
+
+func getLocalVar() int {
+	return localGoldexVar
+}
+
+func setLocalVar(i int) int {
+	localGoldexVar = i
 }

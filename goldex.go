22161502a9adc629c81d2003174964673429dd2d
Jorge <71904707+reriver@users.noreply.github.com>
2022-06-28 11:02:58 +0300

    goldex 2

--- goldex.go
+++ goldex.go
@@ -2,17 +2,17 @@ package goldex
 
 var localGoldexVar int
 
-func return42(i int) int {
+func Return42(i int) int {
 	return 42
 }
-func add42(i int) int {
+func Add42(i int) int {
 	return i + 42
 }
 
-func getLocalVar() int {
+func GetLocalVar() int {
 	return localGoldexVar
 }
 
-func setLocalVar(i int) {
+func SetLocalVar(i int) {
 	localGoldexVar = i
 }
